603d767af1ce53273ea34d3ba90e149aa275e8d1
Jacob Grimes <grime3ja@dukes.jmu.edu>
2026-04-21 20:00:36 -0400

    more categories

--- scripts/main.js
+++ scripts/main.js
@@ -2,9 +2,11 @@ function send_email() {
     emailjs.sendForm('service_lnim8g7', 'template_x8s44g3', "#form").then(
         (response) => {
             console.log('SUCCESS!', response.status, response.text);
+            alert ("Expense logged successfully")
         },
         (error) => {
             console.log('FAILED...', error);
+            alert ("An error occured", error)
         },
     );
 }
